baf9842ef875b752c704292e0880843fd7d33fe1
fdxvv <63183794+fdxvv@users.noreply.github.com>
2023-09-04 20:16:34 +0300

    new file added sample.c

---
+++ sample.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    //---So, this is a change made under the 'new_branch' branch
+    printf("Enter an integer: \n");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d\n", number);
+    
+    return 0;
+}
